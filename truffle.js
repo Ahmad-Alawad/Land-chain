ef3bcc6fa5dd7a61c8e0b3df84a82535caa39cac
Ahmad Alawad <ahmad.alawad.sf@gmail.com>
2018-09-01 15:40:43 +0300

    App was deployed

--- truffle.js
+++ truffle.js
@@ -3,10 +3,10 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      from: '0xD87a4D99EDd08Fde1F461D4FF29C13181177d285',
+      from: '0xC69965a7e9034BD63e2955A2FcBC53a92D97568f',
       network_id: 5777, // Match any network id
-       gasPrice: 50,   // <--- Twice as much
-       gas: 99999999,   // <--- Twice as much
+       gasPrice: 1,   // <--- Twice as much
+       gas: 9999999,   // <--- Twice as much
     }
   }
 };
